1b8dd673badd589699cfd5a26a57a7f0f85946b4
BenHsu501 <stat0v9456reg@gmail.com>
2024-06-24 17:57:43 +0800

    * fix(main_instruction.yaml): update last paragraph to remove author's advertising and private information * fix(main.py): comment out unused command line arguments and add missing import statement * fix(test_expected.yaml): update last paragraph to remove author's advertising and private information * fix(test_utils.py): fix import statement for GetAPIMessage class

--- main.py
+++ main.py
@@ -1,34 +1,54 @@
 import argparse
 import yaml
 from core.utils import GetAPIMessage
+from openai import OpenAI
+
 
 def main():
     parser = argparse.ArgumentParser(description="Data Fetching Operations")
-    parser.add_argument("--mode", choices=["fetch_youtube_playlist", "download_subtitle", 'test'], help="Select the mode of operation.")
-    parser.add_argument("--copywriter_model", default = "PASCA",  help="Select a copywriter model")
+    # parser.add_argument("--mode", choices=["fetch_youtube_playlist", "download_subtitle", 'test'], help="Select the mode of operation.")
+
+
+    parser.add_argument("--copywriter_model", default="PASCA", help="Select the copywriter model to use for generating content.")
+    parser.add_argument("--language", default="English", help="Specify the language for the article.")
+    parser.add_argument("--word_count", type=int, default=300, help="Set the total number of words for the article.")
+    parser.add_argument("--paragraph", default='4-7', help="Define the number of paragraphs in the article.")
+    parser.add_argument("--sentence", type=int, default=3, help="Set the number of sentences per paragraph.")
+    parser.add_argument("--format", type=str, default='markdown', help="Choose the format of the article output.")
+
+
+
 
     args = parser.parse_args()
 
-    if args.mode == 'test':
-        #print(get_copywriter_model('PAsS'))
-        #print(get_assistant('Angel investor'))
-        # print(get_system(role = 'Angel investor'))
-        para = {
-            'copywriter_model': args.copywriter_model,
-            'language': 'English',
-            'word_count': 500,
-            'paragraph': 4,
-            'sentence': 3,
-            'format': 'markdown',
-            'last': ''
-        }
+    para = {
+        'copywriter_model': args.copywriter_model,
+        'language': args.language,
+        'word_count': args.word_count,
+        'paragraph': args.paragraph,
+        'sentence': args.sentence,
+        'format': args.format,
+        'last': ''
+    }
 
-        message = GetAPIMessage(article_type = 'blog', role = 'Angel investor', para = para)
-        # print('1. get_system', message.get_system())
-        # ('2. get_user_main_instruction', message.get_user_main_instruction())
-        print('3 . get_copywriter', message.get_user_copywriter_model())
-        # print('4 . get_my_reference', message.get_my_reference())
-        # print('5. combine_messages', message.combine_messages())
+    message = GetAPIMessage(article_type = 'blog', role = 'Angel investor', para = para)
+    #print(message.combine_messages())
+    # gpt-4o
+    client = OpenAI()
+    response = client.chat.completions.create(model="gpt-3.5-turbo", messages = message.combine_messages(),  max_tokens=2000)
+    '''
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Who won the world series in 2020?"},
+            {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+            {"role": "user", "content": "Where was it played?"}
+        ]
+    )       
+    '''
+    #print(response)
+    print('Text, ', response.choices[0].message.content)
 
 if __name__ == "__main__":
     main()
